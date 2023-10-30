3c0020296a7d9a560fa4586f7225876bbd58c790
Lucas Perdigão De Oliveira <116082415+LucasPerolive@users.noreply.github.com>
2023-10-30 11:06:15 -0300

    Update joao-db.sql

--- banco-de-dados/joao-db.sql
+++ banco-de-dados/joao-db.sql
@@ -1,6 +1,10 @@
 CREATE DATABASE calculadora_materiais;
 USE calculadora_materiais;
 
+CREATE USER 'aplicacao'@'localhost' IDENTIFIED BY '123';
+GRANT ALL PRIVILEGES ON *.* TO 'aplicacao'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+
 CREATE TABLE cliente(
 id INT NOT NULL AUTO_INCREMENT,
 nome CHAR(60) NOT NULL,
